cd287f0d8ccc961deef9717a0a6494f5886a8c39
AmericanEagle1776 <bonnlass@hu-berlin.de>
2018-11-27 17:06:25 +0100

    phasplot is working

--- phaseplot.py
+++ phaseplot.py
@@ -21,10 +21,10 @@ rane = np.linspace(0, 5, 1000)
 if __name__ == '__main__':
     fig1 = plt.figure()
     sub1 = fig1.add_subplot(111)
-    sub1.plot(rane, n1(rane), label='n1, $\lambda = 1$')
+    sub1.plot(rane, n1(rane), label='n1')
+    sub1.plot(rane, n2(rane), label='n2, $\lambda = 1$')
     sub1.plot(rane, n2(rane, 2), label='n2, $\lambda = 2$')
     sub1.plot(rane, n2(rane, 3), label='n2, $\lambda = 3$')
-    sub1.plot(rane, n2(rane, 4), label='n2, $\lambda = 4$')
     sub1.plot(rane, [delta_x/beta for x in rane], ls='--', label='threshold for death')
     sub1.set_xlabel('y')
     sub1.set_ylabel('p')
@@ -32,4 +32,9 @@ if __name__ == '__main__':
     sub1.set_xlim((0, 5.3))
     fig1.legend()
     fig1.tight_layout()
+
+    fig2 = plt.figure()
+    sub21 = fig2.add_subplot(111)
+    sub1.plot(rane, n1(rane), label='n1, $\lambda = 1$')
+
     plt.show()
