037f7c3cb9100f8b8e19bf8de515e27cebd1aaf5
AmericanEagle1776 <bonnlass@hu-berlin.de>
2018-11-27 11:41:06 +0100

    parameter

--- ode_system.py
+++ ode_system.py
@@ -1,7 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
+lambd = 0 # wir könnens nicht lambda nennen
+delta_x = 1
+delta_y = 1
+beta = 1
+mu = 1
+kappa = 1
 
 def f_y(y):
+
+def h_p(p):
+
+
+
+
+def f(t, y):
+
