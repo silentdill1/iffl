3836dbf95e2a416c745e9dd7df2402af9a04b42e
AmericanEagle1776 <bonnlass@hu-berlin.de>
2018-11-27 13:45:14 +0100

    parameter werte eingefügt, f definiert

--- ypu.py
+++ ypu.py
@@ -1,26 +1,37 @@
 import numpy as np
 
-#params
-lambd = 0 # wir könnens nicht lambda nennen
-delta_x = 1
-delta_y = 1
-beta = 1
-mu = 1
-kappa = 1
+# params - cell pops in 10^6 cells
+# /day
+lambd = 1  # /day
+delta_x = 0.1  # /day
+delta_y = 0.1  # /day
+beta = 1  # /day
+mu = 10  # /day
+kappa = 10 ** (-5)  # /(day*unit cells)
+V = 0.25  # /day
+K = 100  # * unit cells
+epsilon = 10 ** (-5)  # /(day*unit cells)
 
 # initial vals
 y0 = 0
 p0 = 0
-u0 = 0
+u0 = 1
 x0 = [y0, p0, u0]
 
+
 def f_y(y):
+    """
+    autocatalysis term, function from paper
+    :param y:
+    :return:
+    """
+    return (V * y ** 2 / (K + y)) - epsilon * y ** 2 - delta_y * y
 
 
 def h_p(p):
-    #at first we will use mu* p
-    #todo implement function for h_p and a switch parameter
-    pi = mu*p
+    # at first we will use mu* p
+    # todo implement function for h_p and a switch parameter
+    pi = mu * p
     return pi
 
 
@@ -35,6 +46,6 @@ def f(t, x):
     yi, pi, ui = x
 
     dy = h_p(pi) + f_y(yi)
-    dp = pi*(delta_x + lambd - kappa * yi - beta * pi)
+    dp = pi * (delta_x + lambd - kappa * yi - beta * pi)
     du = (lambd - kappa * yi) * ui
     return [dy, dp, du]
