44ce7e12fe4a85caf45e921068c7b24abbb586c8
AmericanEagle1776 <bonnlass@hu-berlin.de>
2018-11-27 16:48:00 +0100

    phasplot is working

--- phaseplot.py
+++ phaseplot.py
@@ -4,22 +4,32 @@ from ypu_ode import *
 import matplotlib.pyplot as plt
 from plot_ypu import f_y2
 
+
 def n1(y):
     return -f_y2(y) / mu
 
 
-def n2(y):
-    return 1 / beta * (delta_x + lambd - kappa * y)
+kappa = 1
 
 
-rane = np.linspace(0, 100, 1000)
+def n2(y, lambd=1):
+    n = 1 / beta * (delta_x + lambd - kappa * y)
+    return n
 
-fig1 = plt.figure()
-sub1 = fig1.add_subplot(111)
-sub1.plot(rane, n1(rane), label='n1')
-sub1.plot(rane, n2(rane), label='n2')
-sub1.set_xlabel('y')
-sub1.set_ylabel('p')
-fig1.legend()
-fig1.tight_layout()
-plt.show()
+
+rane = np.linspace(0, 5, 1000)
+if __name__ == '__main__':
+    fig1 = plt.figure()
+    sub1 = fig1.add_subplot(111)
+    sub1.plot(rane, n1(rane), label='n1, $\lambda = 1$')
+    sub1.plot(rane, n2(rane, 2), label='n2, $\lambda = 2$')
+    sub1.plot(rane, n2(rane, 3), label='n2, $\lambda = 3$')
+    sub1.plot(rane, n2(rane, 4), label='n2, $\lambda = 4$')
+    sub1.plot(rane, [delta_x/beta for x in rane], ls='--', label='threshold for death')
+    sub1.set_xlabel('y')
+    sub1.set_ylabel('p')
+    sub1.set_ylim((0, 15))
+    sub1.set_xlim((0, 5.3))
+    fig1.legend()
+    fig1.tight_layout()
+    plt.show()
